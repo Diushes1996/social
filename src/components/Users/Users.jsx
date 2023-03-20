41ccd1f09710db2030be43eead25df5134674c42
Diushes1996 <kakakakaktus@inbox.ru>
2023-03-20 21:34:31 +0300

    REST API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,12 +1,19 @@
+import axios from "axios"
+import userWithoutPhoto from "./../../assets/img/userWithoutPhoto.png"
+
 const Users = (props) => {
 
     if (props.users.length === 0) {
-        props.setUsers([
-            { id: 1, photoUrl: 'https://www.mariinsky.ru/images/cms/data/orchestra_biography/violin/smirnov_dmitry2018.jpg', followed: false, fullName: 'Dmitry', status: 'I am a super hero', location: { city: 'Minsk', country: 'Belarus' } },
-            { id: 2, photoUrl: 'https://www.mariinsky.ru/images/cms/data/orchestra_biography/violin/smirnov_dmitry2018.jpg', followed: true, fullName: 'Andrew', status: 'I hate apples', location: { city: 'Moscow', country: 'Russia' } },
-            { id: 3, photoUrl: 'https://www.mariinsky.ru/images/cms/data/orchestra_biography/violin/smirnov_dmitry2018.jpg', followed: false, fullName: 'Sasha', status: 'I am a boss', location: { city: 'Kiev', country: 'Ukraine' } },
-            { id: 4, photoUrl: 'https://www.mariinsky.ru/images/cms/data/orchestra_biography/violin/smirnov_dmitry2018.jpg', followed: true, fullName: 'Alex', status: 'I am a looser', location: { city: 'Baranovichi', country: 'USA' } }
-        ])
+
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+        props.setUsers(response.data.items);
+            // props.setUsers([
+            //     { id: 1, photoUrl: 'https://www.mariinsky.ru/images/cms/data/orchestra_biography/violin/smirnov_dmitry2018.jpg', followed: false, fullName: 'Dmitry', status: 'I am a super hero', location: { city: 'Minsk', country: 'Belarus' } },
+            //     { id: 2, photoUrl: 'https://www.mariinsky.ru/images/cms/data/orchestra_biography/violin/smirnov_dmitry2018.jpg', followed: true, fullName: 'Andrew', status: 'I hate apples', location: { city: 'Moscow', country: 'Russia' } },
+            //     { id: 3, photoUrl: 'https://www.mariinsky.ru/images/cms/data/orchestra_biography/violin/smirnov_dmitry2018.jpg', followed: false, fullName: 'Sasha', status: 'I am a boss', location: { city: 'Kiev', country: 'Ukraine' } },
+            //     { id: 4, photoUrl: 'https://www.mariinsky.ru/images/cms/data/orchestra_biography/violin/smirnov_dmitry2018.jpg', followed: true, fullName: 'Alex', status: 'I am a looser', location: { city: 'Baranovichi', country: 'USA' } }
+            // ])
+        })
     }
 
     return (
@@ -14,14 +21,15 @@ const Users = (props) => {
             {
                 props.users.map(u=> <div key={u.id}>
                     <div>
-                        <img src={u.photoUrl} alt="" />
-                        {u.followed?<button onClick={()=>{props.unfollow(u.id)}}>Unfollow</button>:<button onClick={()=>{props.follow(u.id)}}>Followed</button>}
+                        {u.photos.small?<img src={u.photos.small} alt='userPhoto'/>:<img src={userWithoutPhoto} alt='userPhoto'/>}
+                        {u.followed?<button onClick={()=>{props.unfollow(u.id)}}>Unfollow</button>:<button onClick={()=>{props.follow(u.id)}}>Follow</button>}
                     </div>
                     <div>
-                        <p>{u.fullName}</p>
+                        <p>{u.name}</p>
                         <p>{u.status}</p>
-                        <p>{u.location.city}</p>
-                        <p>{u.location.country}</p>
+                        <p>{u.id}</p>
+                        <p>{"u.location.city"}</p>
+                        <p>{"u.location.country"}</p>
                     </div>
                 </div>)
             }
