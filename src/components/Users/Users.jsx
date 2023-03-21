77f80615f7e8a8a9fa239761a67aff67d20a413c
Diushes1996 <kakakakaktus@inbox.ru>
2023-03-21 23:10:02 +0300

    Fixing side effect

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,22 +2,24 @@ import axios from "axios"
 import userWithoutPhoto from "./../../assets/img/userWithoutPhoto.png"
 
 const Users = (props) => {
+    let getUsers = () => {
+        if (props.users.length === 0) {
 
-    if (props.users.length === 0) {
-
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-        props.setUsers(response.data.items);
-            // props.setUsers([
-            //     { id: 1, photoUrl: 'https://www.mariinsky.ru/images/cms/data/orchestra_biography/violin/smirnov_dmitry2018.jpg', followed: false, fullName: 'Dmitry', status: 'I am a super hero', location: { city: 'Minsk', country: 'Belarus' } },
-            //     { id: 2, photoUrl: 'https://www.mariinsky.ru/images/cms/data/orchestra_biography/violin/smirnov_dmitry2018.jpg', followed: true, fullName: 'Andrew', status: 'I hate apples', location: { city: 'Moscow', country: 'Russia' } },
-            //     { id: 3, photoUrl: 'https://www.mariinsky.ru/images/cms/data/orchestra_biography/violin/smirnov_dmitry2018.jpg', followed: false, fullName: 'Sasha', status: 'I am a boss', location: { city: 'Kiev', country: 'Ukraine' } },
-            //     { id: 4, photoUrl: 'https://www.mariinsky.ru/images/cms/data/orchestra_biography/violin/smirnov_dmitry2018.jpg', followed: true, fullName: 'Alex', status: 'I am a looser', location: { city: 'Baranovichi', country: 'USA' } }
-            // ])
-        })
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            props.setUsers(response.data.items);
+                // props.setUsers([
+                //     { id: 1, photoUrl: 'https://www.mariinsky.ru/images/cms/data/orchestra_biography/violin/smirnov_dmitry2018.jpg', followed: false, fullName: 'Dmitry', status: 'I am a super hero', location: { city: 'Minsk', country: 'Belarus' } },
+                //     { id: 2, photoUrl: 'https://www.mariinsky.ru/images/cms/data/orchestra_biography/violin/smirnov_dmitry2018.jpg', followed: true, fullName: 'Andrew', status: 'I hate apples', location: { city: 'Moscow', country: 'Russia' } },
+                //     { id: 3, photoUrl: 'https://www.mariinsky.ru/images/cms/data/orchestra_biography/violin/smirnov_dmitry2018.jpg', followed: false, fullName: 'Sasha', status: 'I am a boss', location: { city: 'Kiev', country: 'Ukraine' } },
+                //     { id: 4, photoUrl: 'https://www.mariinsky.ru/images/cms/data/orchestra_biography/violin/smirnov_dmitry2018.jpg', followed: true, fullName: 'Alex', status: 'I am a looser', location: { city: 'Baranovichi', country: 'USA' } }
+                // ])
+            })
+        }
     }
 
     return (
         <div>
+            <button onClick={getUsers}>Get users</button>
             {
                 props.users.map(u=> <div key={u.id}>
                     <div>
