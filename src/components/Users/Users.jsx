cf191560a9d92a94e0c29ea8aed90478afd354be
Diushes1996 <kakakakaktus@inbox.ru>
2023-03-26 19:58:29 +0300

    Refactoring container's and presentation's components

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,66 +1,50 @@
-import React from "react"
-import axios from "axios"
-import userWithoutPhoto from "./../../assets/img/userWithoutPhoto.png"
-import s from "./Users.module.css"
+import s from "./Users.module.css";
+import userWithoutPhoto from "./../../assets/img/userWithoutPhoto.png";
 
-class Users extends React.Component {
+let Users = (props) => {
 
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${this.props.currentPage}`).then(response => {
-            this.props.setUsers(response.data.items);
-            this.props.setTotalUsersCount(response.data.totalCount);
-        })
+    console.log(props.onPageChanged)
+
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize); //для показа целочисленного кол-ва страниц
+
+    let pages = [];
+
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i)
     }
 
-    onPageChanged = (pageNumber) => {
-        this.props.setCurrentPage(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${pageNumber}`).then(response => {
-            this.props.setUsers(response.data.items);
-        })
-    }
+    //карусель страниц начало
+    let curP = props.currentPage;
+    let curPF = ((curP - 5) < 0) ? 0 : curP - 5;
+    let curPL = curP + 5;
+    let slicedPages = pages.slice(curPF, curPL);
+    //карусель страниц конец
 
-    render() {
-
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize); //для показа целочисленного кол-ва страниц
-
-        let pages = [];
-
-        for (let i = 1; i <= pagesCount; i++) {
-            pages.push(i)
-        }
-        
-        //карусель страниц начало
-        let curP = this.props.currentPage;
-        let curPF = ((curP - 5) < 0) ? 0 : curP - 5;
-        let curPL = curP + 5;
-        let slicedPages = pages.slice(curPF, curPL);
-        //карусель страниц конец
-
-        return <div>
-            <div>
-                {slicedPages.map(p => {
-                    return <span className={this.props.currentPage === p && s.selectedPage} 
-                    onClick={(e)=>{this.onPageChanged(p)}}>{p}</span>
-                })}
-            </div>
-            <button onClick={this.getUsers}>Get users</button>
-            {
-                this.props.users.map(u => <div key={u.id}>
-                    <div>
-                        {u.photos.small ? <img src={u.photos.small} alt='userPhoto' /> : <img src={userWithoutPhoto} alt='userPhoto' />}
-                        {u.followed ? <button onClick={() => { this.props.unfollow(u.id) }}>Unfollow</button> : <button onClick={() => { this.props.follow(u.id) }}>Follow</button>}
-                    </div>
-                    <div>
-                        <p>{u.name}</p>
-                        <p>{u.status}</p>
-                        <p>{u.id}</p>
-                        <p>{"u.location.city"}</p>
-                        <p>{"u.location.country"}</p>
-                    </div>
-                </div>)
-            }
+    return <div>
+        <div>
+            {slicedPages.map(p => {
+                return <span className={props.currentPage === p && s.selectedPage}
+                    onClick={(e) => { props.onPageChanged(p)}}>
+                    {p}
+                </span>
+            })}
         </div>
-    }
+        {
+            props.users.map(u => <div key={u.id}>
+                <div>
+                    {u.photos.small ? <img src={u.photos.small} alt='userPhoto' /> : <img src={userWithoutPhoto} alt='userPhoto' />}
+                    {u.followed ? <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button> : <button onClick={() => { props.follow(u.id) }}>Follow</button>}
+                </div>
+                <div>
+                    <p>{u.name}</p>
+                    <p>{u.status}</p>
+                    <p>{u.id}</p>
+                    <p>{"u.location.city"}</p>
+                    <p>{"u.location.country"}</p>
+                </div>
+            </div>)
+        }
+    </div>
 }
 
 export default Users;
